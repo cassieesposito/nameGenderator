913d2408ecb0f2721fb24dfbe8be9f6db1ae72ba
Cassie Esposito <dev@timevault.org>
2021-12-28 02:38:13 -0800

    Cleaning up remnants of abandoned fully local JavaScript approach 	deleted:    cgi-bin/data 	modified:   cgi-bin/deploy.sh 	deleted:    cgi-bin/index.js

--- cgi-bin/deploy.sh
+++ cgi-bin/deploy.sh
@@ -1,5 +1,7 @@
 gcloud functions deploy 'nameGenderator'\
- --runtime 'nodejs16'\
- --allow-unauthenticated
+ --runtime 'python39'\
+ --service-account='name-genderator@portfolio-334101.iam.gserviceaccount.com'\
+ --allow-unauthenticated\
  --trigger-http\
  --entry-point 'main'\
+ --region='us-west1'
