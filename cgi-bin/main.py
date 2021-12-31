a587714e0a7b92ab5489542a9fc63777f918eda8
Cassie Esposito <dev@timevault.org>
2021-12-30 21:29:15 -0800

    Organized functions 	modified:   cgi-bin/main.py

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -7,19 +7,30 @@ def main(request):
     if request.method == "OPTIONS":
         return corsInfo()
 
-    if "name" not in request.args:
-        return 'Error 400: Parameter "name" is required.', 400
+    if "name" in request.args:
+        return httpGetResponse(request.args.get("name"))
 
-    name = str.capitalize(request.args.get("name"))
+    return 'Error 400: Parameter "name" is required.', 400
+
+
+def httpGetResponse(name):
+    nameData = queryDatabase(str.capitalize(name))
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+    }
+
+    return json.dumps(nameData), 200, headers
+
+
+def queryDatabase(name):
     nameData = {name: {}}
-
     with MODELS.ENGINE.connect() as conn:
         stmt = select(MODELS.TABLE).where(MODELS.TABLE.c.name == name)
         for row in conn.execute(stmt):
             nameData[name][row.year] = {"female": row.female, "male": row.male}
 
-    headers = {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"}
-    return json.dumps(nameData), 200, headers
+    return nameData
 
 
 def corsInfo():
